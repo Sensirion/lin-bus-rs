bf787f5f4a989b7f7db45ff4d6e43da7ac23d041
Raphael Nestler <raphael.nestler@sensirion.com>
2018-12-19 12:38:21 +0100

    Update to Rust 2018 syntax
    Just ran `cargo fix --edition` and `cargo fmt`.

--- src/driver.rs
+++ src/driver.rs
@@ -1,9 +1,9 @@
 ///! Trait for a hardware driver to implement
-pub use Error;
-use PID;
+pub use crate::Error;
+use crate::PID;
 
 pub trait Master {
-    type Error: Into<::Error> + From<::Error>;
+    type Error: Into<crate::Error> + From<crate::Error>;
     fn send_wakeup(&mut self) -> Result<(), Self::Error>;
     fn send_header(&mut self, pid: PID) -> Result<(), Self::Error>;
     fn read(&mut self, buf: &mut [u8]) -> Result<(), Self::Error>;
